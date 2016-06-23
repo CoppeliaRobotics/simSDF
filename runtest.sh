8f3b03c07ce00dbaeabc797ebe3027b01a92ffa8
Federico Ferri <federico.ferri.it@gmail.com>
2016-06-23 14:12:30 +0200

    add timeout

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 inst_dir="../../vrep.app/Contents/MacOS"
 scene_file="$(cd "$(dirname "$0")"; pwd)/runtest.ttt"
-make debug && cp libv_repExtSDF.1.0.0.dylib "$inst_dir" && cd "$inst_dir" && ./vrep -s -q -g"$1" "$scene_file"
+make debug && cp libv_repExtSDF.1.0.0.dylib "$inst_dir" && cd "$inst_dir" && ./vrep -s500 -q -g"$1" "$scene_file"
