83b6d570dcf7e2fb28f76bb08416983145d30c3d
Federico Ferri <federico.ferri.it@gmail.com>
2016-06-24 14:50:42 +0200

    optionally run test in lldb

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 inst_dir="../../vrep.app/Contents/MacOS"
 scene_file="$(cd "$(dirname "$0")"; pwd)/runtest.ttt"
-make debug && cp libv_repExtSDF.1.0.0.dylib "$inst_dir" && cd "$inst_dir" && ./vrep -s500 -q -g"$1" "$scene_file"
+timeout=500
+make debug && cp libv_repExtSDF.1.0.0.dylib "$inst_dir" && cd "$inst_dir" && if [[ $DEBUG -gt 0 ]]; then lldb ./vrep -- -s$timeout -q -g"$1" "$scene_file"; else ./vrep -s$timeout -q -g"$1" "$scene_file"; fi
