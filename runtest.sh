ca0609179782c77a3006d237bc588765273a24ce
Federico Ferri <federico.ferri.it@gmail.com>
2016-07-01 20:37:01 +0200

    read ONLYCOPY env var to skip running the test

--- runtest.sh
+++ runtest.sh
@@ -2,4 +2,4 @@
 inst_dir="../../vrep.app/Contents/MacOS"
 scene_file="$(cd "$(dirname "$0")"; pwd)/runtest.ttt"
 timeout=500
-make debug && cp libv_repExtSDF.1.0.0.dylib "$inst_dir" && cd "$inst_dir" && if [[ $DEBUG -gt 0 ]]; then lldb --batch --one-line run ./vrep -- -s$timeout -q -g"$1" "$scene_file"; else ./vrep -s$timeout -q -g"$1" "$scene_file"; fi
+make debug && cp -v libv_repExtSDF.1.0.0.dylib "$inst_dir" && cd "$inst_dir" && if [[ $DEBUG -gt 0 ]]; then lldb --batch --one-line run ./vrep -- -s$timeout -q -g"$1" "$scene_file"; elif [[ "x$ONLYCOPY" = "x" ]]; then ./vrep -s$timeout -q -g"$1" "$scene_file"; fi
