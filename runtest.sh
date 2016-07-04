8d769a8fe93c2ec6d1ae1608125f0cd986a8cec8
Federico Ferri <federico.ferri.it@gmail.com>
2016-07-04 15:49:47 +0200

    canonicalize path inside runtest script

--- runtest.sh
+++ runtest.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 inst_dir="../../vrep.app/Contents/MacOS"
 scene_file="$(cd "$(dirname "$0")"; pwd)/runtest.ttt"
+test_file="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 timeout=500
-make debug && cp -v libv_repExtSDF.1.0.0.dylib "$inst_dir" && cd "$inst_dir" && if [[ $DEBUG -gt 0 ]]; then lldb --batch --one-line run ./vrep -- -s$timeout -q -g"$1" "$scene_file"; elif [[ "x$ONLYCOPY" = "x" ]]; then ./vrep -s$timeout -q -g"$1" "$scene_file"; fi
+make debug && cp -v libv_repExtSDF.1.0.0.dylib "$inst_dir" && cd "$inst_dir" && if [[ $DEBUG -gt 0 ]]; then lldb --batch --one-line run ./vrep -- -s$timeout -q -g"$test_file" "$scene_file"; elif [[ "x$ONLYCOPY" = "x" ]]; then ./vrep -s$timeout -q -g"$test_file" "$scene_file"; fi
