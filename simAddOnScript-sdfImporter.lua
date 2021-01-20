f4ae5eb4667baa0b09e9491416cef3aa2cb0ccaf
Federico Ferri <federico.ferri.it@gmail.com>
2021-01-20 13:58:43 +0100

    fix several issues

--- simAddOnScript-sdfImporter.lua
+++ simAddOnScript-sdfImporter.lua
@@ -7,54 +7,37 @@ function importSDF()
 end
 
 function closeDialog()
-    simUI.destroy(ui)
-    ui=nil
-end
-
-function val2bool(v)
-    if v==0 then return false else return true end
+    if ui then
+        simUI.destroy(ui)
+        ui=nil
+    end
+    done=true
 end
 
 function updateOptions(ui,id,val)
-    if id==10 then
-        options.ignoreMissingValues=val2bool(val)
-    elseif id==20 then
-        options.hideCollisionLinks=val2bool(val)
-    elseif id==30 then
-        options.hideJoints=val2bool(val)
-    elseif id==40 then
-        options.convexDecompose=val2bool(val)
-    elseif id==50 then
-        options.showConvexDecompositionDlg=val2bool(val)
-    elseif id==60 then
-        options.createVisualIfNone=val2bool(val)
-    elseif id==70 then
-        options.centerModel=val2bool(val)
-    elseif id==80 then
-        options.prepareModel=val2bool(val)
-    elseif id==90 then
-        options.noSelfCollision=val2bool(val)
-    elseif id==100 then
-        options.positionCtrl=val2bool(val)
+    local function val2bool(v)
+        if v==0 then return false else return true end
     end
-end
-
-function bool2string(b)
-    if b then return "true" else return "false" end
-end
-
-function checkbox(id,text,varname)
-    return '<checkbox id="'..id..'" checked="'..bool2string(options[varname])..'" text="'..text..'" on-change="updateOptions" />\n'
-end
-
-function button(text,fn)
-    return '<button text="'..text..'" on-click="'..fn..'" />\n'
+    if optionsInfo[id] then
+        options[optionsInfo[id].key]=val2bool(val)
+    end
 end
 
 function sysCall_init()
-    if ui then
-        closeDialog()
-    end
+    closeDialog()
+
+    optionsInfo={
+        [10]={name='Ignore missing values',key='ignoreMissingValues'},
+        [20]={name='Hide collision links',key='hideCollisionLinks'},
+        [30]={name='Hide joints',key='hideJoints'},
+        [40]={name='Convex decompose',key='convexDecompose'},
+        [50]={name='Show convex decomp. dlg',key='showConvexDecompositionDlg'},
+        [60]={name='Create visual if none',key='createVisualIfNone'},
+        [70]={name='Center model',key='centerModel'},
+        [80]={name='Prepare model',key='prepareModel'},
+        [90]={name='No self-collision',key='noSelfCollision'},
+        [100]={name='Position ctrl',key='positionCtrl'},
+    }
 
     options={
         ignoreMissingValues=false,
@@ -73,23 +56,26 @@ function sysCall_init()
     local fileName=sim.fileDialog(sim.filedlg_type_load,'Import SDF...',scenePath,'','SDF file','sdf')
 
     if fileName then
+        done=false
         options.fileName=fileName
-        ui=simUI.create(
-            '<ui modal="true" layout="vbox" title="Importing '..fileName..'..." closeable="true" on-close="closeDialog">\n'..
-            checkbox("10","Ignore missing values",'ignoreMissingValues')..
-            checkbox("20","Hide collision links",'hideCollisionLinks')..
-            checkbox("30","Hide joints",'hideJoints')..
-            checkbox("40","Convex decompose",'convexDecompose')..
-            checkbox("50","Show convex decomp. dlg",'showConvexDecompositionDlg')..
-            checkbox("60","Create visual if none",'createVisualIfNone')..
-            checkbox("70","Center model",'centerModel')..
-            checkbox("80","Prepare model",'prepareModel')..
-            checkbox("90","No self-collision",'noSelfCollision')..
-            checkbox("100","Position ctrl",'positionCtrl')..
-            button('Import','importSDF')..
-            '</ui>'
-        )
+        local function checkbox(id,text,varname)
+        end
+        local xml='<ui modal="true" layout="vbox" title="Importing '..fileName..'..." closeable="true" on-close="closeDialog">\n'
+        for id,o in pairs(optionsInfo) do
+            xml=xml..'<checkbox id="'..id..'" checked="'..(options[o.key] and 'true' or 'false')..'" text="'..o.name..'" on-change="updateOptions" />\n'
+        end
+        xml=xml..'<button text="Import" on-click="importSDF" />\n'
+        xml=xml..'</ui>'
+        ui=simUI.create(xml)
     end
+end
 
-    return sim.syscb_cleanup
+function sysCall_nonSimulation()
+    if done then
+        return sim.syscb_cleanup
+    end
+end
+
+function sysCall_cleanup()
+    closeDialog()
 end
